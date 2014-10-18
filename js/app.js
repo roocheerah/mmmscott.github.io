1c6f3d506d8dd82c1ca0fa74d3c321fe69e0e593
mmmscott <mmmscott@uw.edu>
2014-10-18 03:17:56 -0700

    continual fb testing

--- js/app.js
+++ js/app.js
@@ -27,7 +27,7 @@ function initialize() {
     
     function findEvents() {
         FB.api('/search?q="98105"&type=event', function(response) {
-            var data = JSON.parse(response);
+            console.log(response);
         });
     }
 
